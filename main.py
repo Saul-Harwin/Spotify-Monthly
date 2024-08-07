551121190b720dbfd438423629afcc8e9c9ebb8a
Saul Harwin <51516488+Saul-Harwin@users.noreply.github.com>
2024-08-07 23:29:19 +0100

    Auth Code Request

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-sdf
+CLIENT_ID = "a450350db6d64e75b8bde3305d7c5ff9"
+CLIENT_SECRET = "40ca4a8f66304ab0abe7ac56d13523c3"
+REDIRECT_URI = "http://localhost:3000"
+
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+
+import requests
+
+auth_code = requests.get(AUTH_URL, {
+    'client_id': CLIENT_ID,
+    'response_type': 'code',
+    'redirect_uri': 'https://open.spotify.com/collection/playlists',
+    'scope': 'playlist-modify-private',
+})
+print(auth_code)
